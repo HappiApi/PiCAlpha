c1a928f80dd43ea3553dc9af36e1af1ee9857100
Daniel Gavrilov <drifter4e@gmail.com>
2015-06-16 17:35:51 +0100

    Use DATABASE_URL from environment

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import psycopg2
 import urlparse
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse("postgres://vdbennzvevmumz:4IyA_mZ8eHxsKN1go1yrpUa_FI@ec2-54-204-20-209.compute-1.amazonaws.com:5432/d9js65ocv8ilj7")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 conn = psycopg2.connect(
     database=url.path[1:],
