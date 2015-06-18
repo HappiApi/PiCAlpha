7fc263aaac78f265eb323c57fe88d0b5f4e2baaf
HappiApi <apinanhasthanasombat@Apinans-MacBook-Air.local>
2015-06-18 10:56:20 +0100

    Add Admissions

--- hello.py
+++ hello.py
@@ -16,16 +16,18 @@ def schools():
     pic_code = request.args.get('pic_code')
     if valid_pic(pic_code):
         cursor = conn.cursor()
-        cursor.execute('SELECT name, type FROM school WHERE pic_code={0};'.format(pic_code))
+        cursor.execute('SELECT name, type, admission FROM schools WHERE pic_code={0};'.format(pic_code))
         data = cursor.fetchone()
         if data:
             name = data[0]
             school_type = data[1]
+            admission = data[2]
         cursor.close()
         return jsonify(
             pic_code=pic_code,
             name=name,
             type=school_type,
+            admission=admission
         )
     else:
         return jsonify(error='pic_code does not exist.')
